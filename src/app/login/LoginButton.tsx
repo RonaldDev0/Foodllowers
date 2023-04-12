eab486c46f909edd0afb70dbb21a08809622ae25
Ronald <ronaldzamoradev@gmail.com>
2023-04-12 18:13:16 -0500

    fix: button height

--- src/app/login/LoginButton.tsx
+++ src/app/login/LoginButton.tsx
@@ -6,7 +6,7 @@ import { provider } from './providers'
 export default function LoginButon ({ name, logo, login }: provider) {
   return (
     <div className='flex w-full justify-center'>
-      <button className='w-72 flex items-center gap-4 bg-[#B3B3B3] text-xl rounded-xl p-2 [@media(min-width:800px)]:text-white [@media(min-width:800px)]:bg-[#2f2f2f]'>
+      <button className='w-72 flex items-center gap-4 h-16 bg-[#B3B3B3] text-xl rounded-xl p-2 [@media(min-width:800px)]:text-white [@media(min-width:800px)]:bg-[#2f2f2f]'>
         <Image src={logo} alt={name} width='50' height='50' />
         <p>Login with {name}</p>
       </button>
