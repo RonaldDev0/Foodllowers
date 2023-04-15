0c56a7c541fc4e07b9ea9fc75ba6ca4d6949fa9a
Ronald <ronaldzamoradev@gmail.com>
2023-04-14 20:13:10 -0500

    feat: button login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,7 +12,7 @@ export default function Login () {
       <Image src='./foodllowers-logo.svg' width='420' height='420' alt='logo' priority className='rounded-2xl bg-white [@media(min-width:800px)]:translate-y-40' />
       <div className='h-96 flex flex-col justify-around mb-28 mt-14 [@media(min-width:800px)]:bg-[#8D8D8D] [@media(min-width:800px)]:m-0 [@media(min-width:800px)]:w-[650px] [@media(min-width:800px)]:h-[650px] [@media(min-width:800px)]:pt-56 [@media(min-width:800px)]:rounded-xl [@media(min-width:800px)]:justify-normal [@media(min-width:800px)]:gap-10'>
         {
-          providers?.map(({ name, logo, provider }: provider) => <LoginButton key={name} name={name} logo={logo} provider={provider} />)
+          providers.map(({ name, logo, provider }: provider) => <LoginButton key={name} name={name} logo={logo} provider={provider} />)
         }
       </div>
     </div>
