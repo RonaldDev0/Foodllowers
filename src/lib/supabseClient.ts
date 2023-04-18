1b5b09e5d7ba483944aebfba358a9838e1628f65
Ronald <ronaldzamoradev@gmail.com>
2023-04-18 13:13:24 -0500

    env variables

--- src/lib/supabseClient.ts
+++ src/lib/supabseClient.ts
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// const url: any = process.env.NEXT_PUBLIC_SUPABASE_URL
-// const key: any = process.env.NEXT_PUBLIC_SUPABSE_ANON_KEY
+const url: any = process.env.SUPABASE_URL
+const key: any = process.env.SUPABSE_ANON_KEY
 
-// export const supabase = createClient(url, key)
-
-export const supabase = createClient('https://gtsjuxikwdifunrkhpyp.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0c2p1eGlrd2RpZnVucmtocHlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODEyNjQyOTAsImV4cCI6MTk5Njg0MDI5MH0.2HeeYbOngoswMTVz3kC2Dkid8Q5DuJvycyMnptitggE')
+export const supabase = createClient(url, key)
