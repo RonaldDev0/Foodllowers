d006e5fdd4fb8764573d501120cca34507267654
Ronald <ronaldzamoradev@gmail.com>
2023-04-19 14:26:00 -0500

    feat: profile fage (mobile screen)

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -11,13 +11,13 @@ export default function Profile () {
   }, [])
   return (
     <div className='w-full h-screen flex justify-center items-center'>
-      <div className='bg-[#D9D9D9] bg-opacity-10 rounded-xl w-[1200px] h-[400px] flex items-center'>
-        <Image src={user?.avatar_url} alt='avatar' width={300} height={300} className='rounded-full h-[300px] mx-48' />
+      <div className='bg-[#D9D9D9] bg-opacity-10 rounded-xl [@media(min-width:800px)]:w-[1200px] [@media(min-width:800px)]:h-[400px] flex [@media(max-width:800px)]:flex-col items-center gap-44 [@media(min-width:800px)]:p-48 [@media(max-width:800px)]:flex [@media(max-width:800px)]:justify-center [@media(max-width:800px)]:w-full [@media(max-width:800px)]:h-screen'>
+        <Image src={user?.avatar_url} alt='avatar' width={300} height={300} className='rounded-full h-[300px]' />
         <div>
           <span className='text-[#A8A8A8]'>Name</span>
-          <h1>{user?.full_name}</h1>
+          <h1 className='text-xl'>{user?.full_name}</h1>
           <span className='text-[#A8A8A8]'>Email</span>
-          <p>{user?.email}</p>
+          <p className='text-xl'>{user?.email}</p>
         </div>
       </div>
     </div>
