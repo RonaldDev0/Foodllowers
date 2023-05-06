8819b371be8cfaa02ad18ea95fe31e781a22e1cf
Ronald <ronaldzamoradev@gmail.com>
2023-05-06 17:05:50 -0500

    feat:404 page

--- src/app/[error]/page.tsx
+++ src/app/[error]/page.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
 export default function NotFound () {
   return (
-    <div className='w-full h-screen flex justify-center items-center'>
-      <h1>404</h1>
+    <div className='w-full h-screen flex flex-col gap-10 justify-center items-center'>
+      <h1 className='text-4xl'>404</h1>
+      <Image src='img/pato404.svg' alt='404 error' width='400' height='400' />
+      <h2 className='text-2xl'>Page not found</h2>
+      <Link className='text-xl text-green-600' href='/'>Back to Home</Link>
     </div>
   )
 }
--- src/app/adresses/AdressCard.tsx
+++ src/app/adresses/AdressCard.tsx
@@ -6,6 +6,9 @@ type IAdress = {
 }
 
 export default function AdressCard ({ name, adress }: IAdress) {
+  // const delete = () => {}
+  // const edit = () => {}
+
   return (
     <div className='bg-[#D9D9D9] text-black m-5 p-2 rounded w-[330px] flex justify-between'>
       <div>
@@ -13,8 +16,8 @@ export default function AdressCard ({ name, adress }: IAdress) {
         <p className='text-lg font-medium'>{adress}</p>
       </div>
       <div className='flex items-center'>
-        <Image className='cursor-pointer' width='25' height='25' src='./icons/edit.svg' alt='edit' />
-        <Image className='m-3 cursor-pointer' src='./icons/x.svg' alt='close-icon' width='20' height='20' priority />
+        <Image className='cursor-pointer' width='25' height='25' src='./icons/edit.svg' alt='edit' priority />
+        <Image className='m-3 cursor-pointer' width='25' height='25' src='./icons/x.svg' alt='close-icon' priority />
       </div>
     </div>
   )
