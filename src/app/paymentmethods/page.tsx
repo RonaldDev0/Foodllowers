2df605978e591fa659b024a75a8de1db3995638d
Ronald <ronaldzamoradev@gmail.com>
2023-05-18 20:34:52 -0500

    feat: paymenthMethods page

--- src/app/paymentmethods/page.tsx
+++ src/app/paymentmethods/page.tsx
@@ -1,27 +1,18 @@
-import Card from './Card'
+'use client'
+import { useState } from 'react'
 
-export type ICard = {
-  CardNumber: number
-}
+import AddModal from './AddModal'
+import ListCards from './ListCards'
+import AddButton from './AddButton'
 
-export default function PaymentShiptment () {
-  const cardList: ICard[] = [
-    {
-      CardNumber: 400012356789010
-    },
-    {
-      CardNumber: 400012356789010
-    },
-    {
-      CardNumber: 400012356789010
-    }
-  ]
+export default function PaymentMethods () {
+  const [open, setOpen] = useState<boolean>(false)
 
   return (
-    <div className='w-full h-screen flex flex-col gap-10 justify-center items-center'>
-      {
-        cardList.map(({ CardNumber }) => <Card key={CardNumber} CardNumber={CardNumber} />)
-      }
+    <div className='w-full h-screen flex flex-col top-12 justify-around items-center'>
+      {open && <AddModal setOpen={setOpen} />}
+      <ListCards />
+      <AddButton setOpen={setOpen} />
     </div>
   )
 }
--- src/context/user.tsx
+++ src/context/user.tsx
@@ -8,14 +8,18 @@ type IContext = {
   adresses: any
   setAdresses: Function
   userId: any
+  cards: any
+  setCards: Function
 }
 
 const Context = createContext<IContext>({
   user: undefined,
   setUser: Function,
-  adresses: {},
+  adresses: undefined,
   setAdresses: Function,
-  userId: ''
+  userId: '',
+  cards: undefined,
+  setCards: Function
 })
 
 export function UserProvider ({ children }: { children: ReactNode }) {
@@ -23,7 +27,7 @@ export function UserProvider ({ children }: { children: ReactNode }) {
   const [user, setUser] = useState()
   const [userId, setUserId] = useState()
   const [adresses, setAdresses] = useState<any>()
-  // const [payments, setPayments] = useState()
+  const [cards, setCards] = useState<any>()
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session: { user } } }: any) => {
@@ -32,10 +36,13 @@ export function UserProvider ({ children }: { children: ReactNode }) {
     })
 
     supabase.from('adresses').select('*').order('id').then(({ data }) => setAdresses(data))
+    supabase.from('cards').select('*').order('id').then(({ data }) => setCards(data))
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
-    <Context.Provider value={{ user, setUser, adresses, setAdresses, userId }}>
+    <Context.Provider value={{ user, setUser, adresses, setAdresses, userId, cards, setCards }}>
       <>{children}</>
     </Context.Provider>
   )
