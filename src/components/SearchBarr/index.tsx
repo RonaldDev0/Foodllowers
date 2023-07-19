90bbba263b6ae75acf7a0b6faf82412af648e38a
Ronald Zamora <80486521+RonaldDev0@users.noreply.github.com>
2023-07-19 11:59:42 -0500

    Update index.tsx

--- src/components/SearchBarr/index.tsx
+++ src/components/SearchBarr/index.tsx
@@ -1,11 +1,9 @@
-
 import Image from 'next/image'
 export function SearchBarr () {
   return (
     <div className='mt-24 flex flex-col items-center'>
       <p className='text-xl m-2'>¿Cuál es tu influenciador favorito?</p>
       <form className='flex'>
-  
         <button className='bg-dark_bg p-3 rounded-r-lg'>
           <Image src='./icons/search.svg' width='30' height='30' alt='search.svg' />
         </button>
