cb01a2093b7a8a7e0ef6670be5c031529a0b3c41
Ronald Zamora <80486521+RonaldDev0@users.noreply.github.com>
2023-07-19 11:58:32 -0500

    Update index.tsx

--- src/components/SearchBarr/index.tsx
+++ src/components/SearchBarr/index.tsx
@@ -1,20 +1,10 @@
-'use client'
 
 import Image from 'next/image'
-import { useState } from 'react'
-import { useContent } from '@/context'
-
 export function SearchBarr () {
-  const { influencerList, setSearchFilter } = useContent()
-
-  const handleSubmit = (e: any) => {
-    e.preventDefault()
-  }
-
   return (
     <div className='mt-24 flex flex-col items-center'>
       <p className='text-xl m-2'>¿Cuál es tu influenciador favorito?</p>
-      <form className='flex' onSubmit={handleSubmit}>
+      <form className='flex'>
   
         <button className='bg-dark_bg p-3 rounded-r-lg'>
           <Image src='./icons/search.svg' width='30' height='30' alt='search.svg' />
