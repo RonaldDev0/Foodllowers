efd3e82fc54ced184251b907e70efcb88744c97c
Ronald Zamora <80486521+RonaldDev0@users.noreply.github.com>
2023-07-19 11:54:44 -0500

    test: inputs (Mac OS)

--- src/components/SearchBarr/index.tsx
+++ src/components/SearchBarr/index.tsx
@@ -17,7 +17,7 @@ export function SearchBarr () {
     <div className='mt-24 flex flex-col items-center'>
       <p className='text-xl m-2'>¿Cuál es tu influenciador favorito?</p>
       <form className='flex' onSubmit={handleSubmit}>
-        <input onChange={(e: any) => setInput(e.target.value)} value={input} className='bg-dark_bg outline-none p-3 w-[500px] [@media(min-width:800px)]:focus:w-[600px] transition-all rounded-l-lg [@media(max-width:800px)]:w-[300px]' placeholder='Buscar...' type='text' />
+        
         <button className='bg-dark_bg p-3 rounded-r-lg'>
           <Image src='./icons/search.svg' width='30' height='30' alt='search.svg' />
         </button>
