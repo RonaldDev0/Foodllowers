606d4c4ad5a166176737904083cb349a9a3c42c0
Ronald Zamora <80486521+RonaldDev0@users.noreply.github.com>
2023-07-19 11:56:18 -0500

    Update index.tsx

--- src/components/SearchBarr/index.tsx
+++ src/components/SearchBarr/index.tsx
@@ -6,7 +6,6 @@ import { useContent } from '@/context'
 
 export function SearchBarr () {
   const { influencerList, setSearchFilter } = useContent()
-  const [input, setInput] = useState<any>('')
 
   const handleSubmit = (e: any) => {
     e.preventDefault()
@@ -17,7 +16,7 @@ export function SearchBarr () {
     <div className='mt-24 flex flex-col items-center'>
       <p className='text-xl m-2'>¿Cuál es tu influenciador favorito?</p>
       <form className='flex' onSubmit={handleSubmit}>
-        
+  
         <button className='bg-dark_bg p-3 rounded-r-lg'>
           <Image src='./icons/search.svg' width='30' height='30' alt='search.svg' />
         </button>
