5409fa1927af0643ac31ee8a879dfcb00e971312
Ronald Zamora <80486521+RonaldDev0@users.noreply.github.com>
2023-07-24 12:38:11 -0500

    Change tipografy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import { ReactNode } from 'react'
 import { SupabaseProvider } from './supabaseProvider'
 import { UserProvider, ContentProvider } from '@/context'
+import { Quicksand } from 'next/font/google'
 
 // Components
 import { SideBarr } from '@/components'
 
 import 'tailwindcss/tailwind.css'
 import './globals.css'
+
+const quicksand = Quicksand({ subsets: ['vietnamese'] })
 
 export const metadata = {
   title: 'Foodllowers | 🍔 Food By Influencers',
@@ -17,7 +20,7 @@ export const metadata = {
 export default function RootLayout ({ children }: { children: ReactNode }) {
   return (
     <html lang='en'>
-      <body className='bg-bg text-white'>
+      <body className={`${quicksand.className} bg-bg text-white`}>
         <SupabaseProvider>
           <UserProvider>
             <ContentProvider>
