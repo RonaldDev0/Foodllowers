996941f6f63dbdcd65feefb0d31f7f417da93e54
Ronald <ronaldzamoradev@gmail.com>
2023-07-29 11:54:47 -0500

    fix: api

--- src/app/api/hello/route.ts
+++
@@ -1,9 +0,0 @@
-import { useSupabase } from '@/app/supabaseProvider'
-
-export async function GET (request: Request) {
-  const { supabase } = useSupabase()
-  const [adresses] = await Promise.all([
-    supabase.from('adresses').select('*').order('id').then(({ data }) => data)
-  ])
-  return new Response(JSON.stringify(adresses))
-}
