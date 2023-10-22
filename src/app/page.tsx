11eb51b6792ad2c10d97a10707d4ed4cd87c2ef6
Ronald <ronaldzamoradev@gmail.com>
2023-10-22 17:44:45 -0500

    feat: home page performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,60 @@
 'use client'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import Link from 'next/link'
 import Image from 'next/image'
 import { Card, CardBody } from '@nextui-org/react'
 import { SearchBarr } from '@/components'
 import { useSupabase } from './Providers'
-
-type IInfluencer = {
-  id: number
-  qualification: number
-  full_name: string
-  document_number: string
-  gender: string
-  preview: string
-  bank: string
-  path: string
-}
+import { useContent } from '@/store'
 
 export default function Home () {
   const router = useRouter()
   const loginCode = useSearchParams().get('code')
   const { supabase } = useSupabase()
-
-  const [influencerList, setInfluencerList] = useState<any>()
+  const { influencerList, setStore } = useContent()
 
   useEffect(() => {
     loginCode && setTimeout(() => router.push('/'), 200)
 
-    supabase
-      .from('influencers')
-      .select('id, full_name, qualification, preview, path')
-      .range(0, 10)
-      .then(res => setInfluencerList(res.data))
+    !influencerList && (
+      supabase
+        .from('influencers')
+        .select('id, full_name, qualification, preview, path')
+        .range(0, 10)
+        .then(res => setStore('influencerList', res.data))
+    )
   }, [])
 
   return (
     <div className='flex flex-col w-full mb-16 items-center gap-12'>
       <SearchBarr message />
       <div className='flex flex-wrap gap-5 justify-center'>
-        {influencerList?.map(({ id, qualification, full_name: fullName, path, preview }: IInfluencer) => (
-          <Link href={path} key={id}>
-            <Card>
-              <CardBody className='p-0'>
-                <Image src={preview} width='350' height='200' alt='img preview' className='w-[350px] h-[200px]' />
-                <div className='px-4 pb-2 pt-2 flex justify-between'>
-                  <p className='text-xl'>{fullName}</p>
-                  <p>⭐{qualification}</p>
-                </div>
-              </CardBody>
-            </Card>
-          </Link>
-        ))}
+        {
+          influencerList?.map(item => (
+            <Link href={item.path} key={item.id}>
+              <Card>
+                <CardBody className='p-0'>
+                  <Image
+                    src={item.preview}
+                    width='350'
+                    height='200'
+                    alt='img preview'
+                    className='w-[350px] h-[200px]'
+                  />
+                  <div className='px-4 pb-2 pt-2 flex justify-between'>
+                    <p className='text-xl'>
+                      {item.full_name}
+                    </p>
+                    <p>
+                      ⭐{item.qualification}
+                    </p>
+                  </div>
+                </CardBody>
+              </Card>
+            </Link>
+          ))
+        }
       </div>
     </div>
   )
--- src/store/useContent.ts
+++ src/store/useContent.ts
@@ -1,12 +1,25 @@
 import { create } from 'zustand'
 
+type IInfluencer = {
+  id: number
+  qualification: number
+  full_name: string
+  document_number: string
+  gender: string
+  preview: string
+  bank: string
+  path: string
+}
+
 type State = {
+  influencerList: IInfluencer[] | null
 }
 
 type Actions = {
   setStore: (property: keyof State, value: any) => void
 }
 
-export const useUser = create<State & Actions>(set => ({
+export const useContent = create<State & Actions>(set => ({
+  influencerList: null,
   setStore: (property, value) => set(prevState => ({ ...prevState, [property]: value }))
 }))
