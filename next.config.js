607777d9e394980161b8ce785c37a9e70b701106
Ronald <ronaldzamoradev@gmail.com>
2024-02-24 22:12:23 -0500

    fix

--- next.config.js
+++ next.config.js
@@ -2,13 +2,7 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/maps\.googleapis\.com\//,
-      handler: 'NetworkOnly'
-    }
-  ]
+  disable: true
 })
 
 /** @type {import('next').NextConfig} */
