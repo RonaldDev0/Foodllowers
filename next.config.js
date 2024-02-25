0b3c74f0aec2914ee9099171071bd0457ec681d1
Ronald <ronaldzamoradev@gmail.com>
2024-02-24 22:37:50 -0500

    fix: workbox logs

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
+  disable: process.env.NODE_ENV === 'development',
+  workboxOptions: {
+    disableDevLogs: true
+  }
 })
 
 /** @type {import('next').NextConfig} */
