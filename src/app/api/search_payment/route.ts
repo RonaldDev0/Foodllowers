fb2de72e23bc331a3b5010a3d91f4c2fb2815158
Ronald <ronaldzamoradev@gmail.com>
2024-05-09 09:20:45 -0500

    fix: api/search_payment

--- src/app/api/search_payment/route.ts
+++ src/app/api/search_payment/route.ts
@@ -4,6 +4,7 @@ export async function POST (req: NextRequest) {
   const { paymentId } = await req.json()
 
   const { id, status }: any = await fetch(`https://api.mercadopago.com/v1/payments/${paymentId}?access_token=${process.env.MP_ACCESS_TOKEN}`)
+    .then(res => res.json())
 
   return NextResponse.json({ id, status })
 }
