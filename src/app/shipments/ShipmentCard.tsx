25ca732f11b6ea0127364cb59fc9f9c78ac452c9
Ronald <ronaldzamoradev@gmail.com>
2024-05-11 21:12:32 -0500

    refactor: /shipments

---
+++ src/app/shipments/ShipmentCard.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable camelcase */
+import Image from 'next/image'
+import { Card, CardBody, Avatar } from '@nextui-org/react'
+
+export function ShipmentCard ({ shipment }: { shipment: any }) {
+  const { product: { name, price, preview, influencers: { avatar, full_name } } } = shipment
+
+  return (
+    <Card>
+      <CardBody className='p-0'>
+        <div className='flex items-center gap-5 rounded-lg cursor-pointer'>
+          <Image
+            src={preview}
+            alt={name}
+            width={150}
+            height={200}
+            className='h-[150px] w-[200px]'
+          />
+          <div className='mr-5 flex flex-col gap-2'>
+            <p className='text-lg'>
+              {name}
+            </p>
+            <div className='flex gap-2 justify-center items-center'>
+              <Avatar src={avatar} />
+              <p className='opacity-50'>{full_name}</p>
+            </div>
+            <p className='text-green-600'>
+              {price.toLocaleString()}
+            </p>
+          </div>
+        </div>
+      </CardBody>
+    </Card>
+  )
+}
--- src/app/shipments/page.tsx
+++ src/app/shipments/page.tsx
@@ -3,8 +3,7 @@
 import { useEffect } from 'react'
 import { useSupabase } from '../Providers'
 import { useUser } from '@/store'
-import Image from 'next/image'
-import { Card, CardBody, Avatar } from '@nextui-org/react'
+import { ShipmentCard } from './ShipmentCard'
 
 export default function Shipments () {
   const { supabase } = useSupabase()
@@ -29,35 +28,12 @@ export default function Shipments () {
   return (
     <div className='w-full h-screen flex flex-col top-12 justify-around items-center'>
       <div className='flex flex-col gap-4'>
-        {
-          shipmentList?.map(({ id, product: { name, price, preview, influencers: { avatar, full_name } } }) => (
-            <Card key={id}>
-              <CardBody className='p-0'>
-                <div className='flex items-center gap-5 rounded-lg cursor-pointer'>
-                  <Image
-                    src={preview}
-                    alt={name}
-                    width={150}
-                    height={200}
-                    className='h-[150px] w-[200px]'
-                  />
-                  <div className='mr-5 flex flex-col gap-2'>
-                    <p className='text-lg'>
-                      {name}
-                    </p>
-                    <div className='flex gap-2 justify-center items-center'>
-                      <Avatar src={avatar} />
-                      <p className='opacity-50'>{full_name}</p>
-                    </div>
-                    <p className='text-green-600'>
-                      {price.toLocaleString()}
-                    </p>
-                  </div>
-                </div>
-              </CardBody>
-            </Card>
-          ))
-        }
+        {shipmentList?.map((shipment) => (
+          <ShipmentCard
+            key={shipment.id}
+            shipment={shipment}
+          />
+        ))}
       </div>
     </div>
   )
