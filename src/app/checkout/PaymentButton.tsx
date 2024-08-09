c106f6dee500c832e3f2ad5a6462c9e7c652c7c4
Ronald <ronaldzamoradev@gmail.com>
2024-08-09 12:42:39 -0500

    feat: comment the console logs

--- src/app/checkout/PaymentButton.tsx
+++ src/app/checkout/PaymentButton.tsx
@@ -133,7 +133,6 @@ export function PaymentButton ({ amount, error, product, shippingCost, tip, infl
         user,
         addressSelect,
         paymentInfo: {
-          // transaction_amount: 1500,
           transaction_amount: amount,
           callback_url: 'https://foodllowers.vercel.app/currentshipment',
           description: `Foodllowers: ${product.name} - ${product.influencers.full_name}`,
