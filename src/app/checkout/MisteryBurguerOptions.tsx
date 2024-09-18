1fb7c7b29aa67b37a4c8e423fb952b2c13601be2
Ronald <ronaldzamoradev@gmail.com>
2024-09-18 16:06:14 -0500

    feat: ui validations

--- src/app/checkout/MisteryBurguerOptions.tsx
+++ src/app/checkout/MisteryBurguerOptions.tsx
@@ -1,72 +1,137 @@
 'use client'
 import { useState } from 'react'
 import { useSearchParams } from 'next/navigation'
-import { Card, CardBody, CardHeader, CardFooter, Divider, Button, Checkbox, Accordion, AccordionItem } from '@nextui-org/react'
+import { Card, CardBody, CardHeader, Divider, Button, Checkbox, Accordion, AccordionItem, useDisclosure, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter } from '@nextui-org/react'
 import { Info, X } from 'lucide-react'
 
-const categories = [
+type ICategory = {
+  category: string
+  items: {
+    name: string
+    checked: any
+  } []
+}
+
+const initialPreferences: ICategory[] = [
   {
-    title: 'Pan',
-    items: ['Pan brioche a base de papa', 'Pan brioche a base de papa de colores']
-  },
-  {
-    title: 'Carnes',
+    category: 'Pan',
     items: [
-      'Carne 150gr 100% res',
-      'Carne de cerdo desmechada 120gr',
-      'Pechuga de pollo apanada 120gr',
-      'Falafel (opción vegetariana)'
+      { name: 'Pan brioche a base de papa', checked: false },
+      { name: 'Pan brioche a base de papa de colores', checked: false }
     ]
   },
   {
-    title: 'Quesos',
-    items: ['Lonja de queso campesino', 'Queso doble crema tajado']
-  },
-  {
-    title: 'Vegetales',
+    category: 'Carnes',
     items: [
-      'Lechuga romana',
-      'Lechuga crespa',
-      'Cebollín',
-      'Pico de gallo',
-      'Cebolla encurtida',
-      'Cole slaw'
+      { name: 'Carne 150gr 100% res', checked: false },
+      { name: 'Carne de cerdo desmechada 120gr', checked: false },
+      { name: 'Pechuga de pollo apanada 120gr', checked: false },
+      { name: 'Falafel (opción vegetariana)', checked: false }
     ]
   },
   {
-    title: 'Extras',
+    category: 'Quesos',
     items: [
-      'Tocineta',
-      'Piña caramelizada',
-      'Piña en almíbar',
-      'Plátanos maduros',
-      'Totopos'
+      { name: 'Lonja de queso campesino', checked: false },
+      { name: 'Queso doble crema tajado', checked: false }
     ]
   },
   {
-    title: 'Salsas',
+    category: 'Vegetales',
     items: [
-      'Salsa de vino',
-      'Sour cream',
-      'Queso cheddar',
-      'Salsa BBQ',
-      'Tres quesos (de la casa)'
+      { name: 'Lechuga romana', checked: false },
+      { name: 'Lechuga crespa', checked: false },
+      { name: 'Cebollín', checked: false },
+      { name: 'Pico de gallo', checked: false },
+      { name: 'Cebolla encurtida', checked: false },
+      { name: 'Cole slaw', checked: false }
     ]
   },
   {
-    title: 'Acompañantes',
-    items: ['Papas a la francesa', 'Totopos']
+    category: 'Extras',
+    items: [
+      { name: 'Tocineta', checked: false },
+      { name: 'Piña caramelizada', checked: false },
+      { name: 'Piña en almíbar', checked: false },
+      { name: 'Plátanos maduros', checked: false }
+    ]
+  },
+  {
+    category: 'Salsas',
+    items: [
+      { name: 'Salsa de vino', checked: false },
+      { name: 'Sour cream', checked: false },
+      { name: 'Queso cheddar', checked: false },
+      { name: 'Salsa BBQ', checked: false },
+      { name: 'Tres quesos (de la casa)', checked: false }
+    ]
+  },
+  {
+    category: 'Acompañantes',
+    items: [
+      { name: 'Papas a la francesa', checked: false },
+      { name: 'Totopos', checked: false }
+    ]
   }
 ]
 
 export function MisteryBurguerOptions ({ value, setValue }: { value: any, setValue: Function }) {
   const query = useSearchParams().get('q')
   const isMisteryBurguer = query === '471ba020-79b7-4204-9e9d-2e8ca2b0f216'
+  const [preferences, setPreferences] = useState<any>(initialPreferences)
 
-  const [open, setIsOpen] = useState(true)
+  const { isOpen, onOpen, onOpenChange } = useDisclosure()
 
-  const handleSubmit = () => {
-    setIsOpen(false)
+  const [error, setError] = useState<any>(false)
+
+  const handleChange: (category: string, itemName: string) => void = (category, itemName) => {
+    setError(false)
+    setPreferences((prevPreferences: any[]) =>
+      prevPreferences.map((categoryObj) =>
+        categoryObj.category === category
+          ? {
+              ...categoryObj,
+              items: categoryObj.items.map((item: { name: string; checked: any }) =>
+                item.name === itemName
+                  ? { ...item, checked: !item.checked }
+                  : item
+              )
+            }
+          : categoryObj
+      ))
+  }
+
+  function validation () {
+    // almenos un tipo de pan
+    if (preferences[0].items.filter(({ checked }: any) => checked === true).length === preferences[0].items.length) {
+      setError('Debes dejar al menos un tipo de pan disponible.')
+      return true
+    }
+    // minimum 6 ingredients
+    const ingredients = preferences
+      .filter(({ category }: any) => category !== 'Salsas' && category !== 'Acompañantes' && category !== 'Pan')
+      .flatMap(({ items }: ICategory) => items)
+      .filter(({ checked }: any) => checked === false)
+      .length
+
+    if (ingredients < 5) {
+      setError('Debes dejar al menos 5 ingredientes disponibles, sin contar panes, salsas y acompañantes.')
+      return true
+    }
+
+    return false
+  }
+
+  const handleSubmit = (onClose: Function) => {
+    if (validation()) return
+
+    const preferencesFormatted = preferences
+      .map(({ items }: ICategory) => (
+        items.filter(({ checked }: any) => checked).map(({ name }: any) => name)
+      )).flat()
+
+    setValue(preferencesFormatted.length ? preferencesFormatted : null)
+    onClose()
   }
 
   if (!isMisteryBurguer) return null
@@ -77,47 +142,49 @@ export function MisteryBurguerOptions ({ value, setValue }: { value: any, setVal
           <p>Ingredientes a evitar</p>
           <Button
             variant='faded'
-            onPress={() => setIsOpen(true)}
+            onPress={onOpen}
           >
             Cambiar
           </Button>
         </CardHeader>
         <Divider />
-        <CardBody>
-          {value || 'No has escrito nada.'}
+        <CardBody className='max-w-96'>
+          {value?.map((item: string) => (
+            <p key={item}>{item}</p>
+          )) || (
+            <p>No has elegido ingredientes para evitar. <br /> ¡Marca los que prefieres excluir!</p>
+          )}
         </CardBody>
       </Card>
-      {
-        open && (
-          <div className='fixed top-0 left-0 w-full h-full z-50 bg-black bg-opacity-80 flex justify-center items-center'>
-            <Card className='p-2'>
-              <CardHeader className='flex w-full justify-center'>
-                <p>¡Tu Sorpresa Gastronómica!</p>
-              </CardHeader>
-              <Divider />
-              <CardBody className='p-5 rounded-lg flex flex-col gap-5 w-96'>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {onClose => (
+            <>
+              <ModalHeader>
+                ¡Tu Sorpresa Gastronómica!
+              </ModalHeader>
+              <ModalBody>
                 <div className='flex flex-col gap-12'>
                   <div className='flex gap-3 items-center text-sm'>
                     <Info size={60} />
                     <p>Marca los ingredientes que prefieres evitar. <br /> Si no te importa, simplemente deja todas las opciones sin marcar y déjate sorprender.</p>
                   </div>
                   <div>
+                    <p className='text-pink-800 font-bold'>{error || ''}</p>
                     <Accordion>
-                      {categories.map(({ title, items }) => (
-                        <AccordionItem
-                          key={title}
-                          aria-label={title}
-                          title={title}
-                        >
+                      {preferences.map(({ category, items }: any) => (
+                        <AccordionItem key={category} aria-label={category} title={category}>
                           <div className='flex flex-col'>
-                            {items.map(item => (
+                            {items.map(({ name, checked }: any) => (
                               <Checkbox
-                                key={item}
+                                key={name}
                                 color='danger'
                                 lineThrough
                                 icon={<X size={20} />}
+                                isSelected={checked}
+                                onChange={() => handleChange(category, name)}
                               >
-                                {item}
+                                {name}
                               </Checkbox>
                             ))}
                           </div>
@@ -126,20 +193,21 @@ export function MisteryBurguerOptions ({ value, setValue }: { value: any, setVal
                     </Accordion>
                   </div>
                 </div>
-              </CardBody>
-              <CardFooter>
+              </ModalBody>
+              <ModalFooter>
                 <Button
-                  onPress={handleSubmit}
+                  onPress={() => handleSubmit(onClose)}
                   color='secondary'
-                  className='w-full text-lg'
+                  className={`w-full text-lg ${error ? 'opacity-60' : ''}`}
+                  isDisabled={error}
                 >
                   Confirmar
                 </Button>
-              </CardFooter>
-            </Card>
-          </div>
-        )
-      }
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </>
   )
 }
