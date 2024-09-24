94a73d98bd3bb964130151f756c30e8f578f8d1e
Ronald <ronaldzamoradev@gmail.com>
2024-09-24 11:25:46 -0500

    feat: remove costs and rates

--- src/app/checkout/Summary.tsx
+++ src/app/checkout/Summary.tsx
@@ -33,14 +33,12 @@ export function Summary ({ productPrice, serviceFee, shippingCost, tip, total, i
           <div className='text-left'>
             <p>Costo de producto</p>
             <p>Costo de envío</p>
-            <p>Impuestos y Tarifas</p>
             <p>Propina del Delivery</p>
             <p className='font-bold mt-4'>Total</p>
           </div>
           <div className='text-right font-bold text-green-600'>
-            <p>{copFormat(productPrice + serviceFee + influencer)}</p>
+            <p>{copFormat(productPrice + serviceFee + influencer + calculateMercadoPagoComission(total))}</p>
             <p>{copFormat(shippingCost)}</p>
-            <p>{copFormat(calculateMercadoPagoComission(total))}</p>
             <p>{copFormat(tip)}</p>
             <p className='mt-4'>
               {copFormat(total + calculateMercadoPagoComission(total))}
