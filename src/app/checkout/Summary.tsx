e6573ae2b2f5fc53193300162c3ba054783f182d
Ronald <ronaldzamoradev@gmail.com>
2024-11-27 13:16:40 -0500

    feat: 6km => 15km

--- src/app/checkout/Summary.tsx
+++ src/app/checkout/Summary.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Card, CardHeader, CardBody, Divider } from '@nextui-org/react'
+import { Card, CardHeader, CardBody, Divider, Tooltip } from '@nextui-org/react'
 
 type props = {
   shippingCost: number
@@ -28,13 +28,17 @@ export function Summary ({ shippingCost, tip, productPriceWithCoupon }: props) {
         <div className='grid grid-cols-2'>
           <div className='text-left'>
             <p>Costo de producto</p>
-            <p>Costo de envío</p>
+            <Tooltip showArrow content='El costo de envío es proporcional a la distancia de recorrido'>
+              <p>Costo de envío</p>
+            </Tooltip>
             <p>Propina del Delivery</p>
             <p className='font-bold mt-4'>Total</p>
           </div>
           <div className='text-right font-bold text-green-600'>
             <p>{copFormat(productPriceWithCoupon)}</p>
-            <p>{copFormat(shippingCost)}</p>
+            <Tooltip showArrow content='El costo de envío es proporcional a la distancia de recorrido'>
+              <p>{copFormat(shippingCost)}</p>
+            </Tooltip>
             <p>{copFormat(tip)}</p>
             <p className='mt-4'>
               {copFormat(productPriceWithCoupon + tip + shippingCost)}
