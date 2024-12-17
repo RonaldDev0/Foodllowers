efab81549ebb7141288550265bdf63d8b49ec022
Ronald <ronaldzamoradev@gmail.com>
2024-12-17 14:11:02 -0500

    feat: hidden WestCOL

--- src/app/api/content/home/route.ts
+++ src/app/api/content/home/route.ts
@@ -3,10 +3,15 @@ import { supabase } from '@/lib/supabase'
 import { NextResponse } from 'next/server'
 
 export async function GET () {
-  const { data, error } = await supabase
-    .rpc('get_home_data')
+  const { data, error } = await supabase.rpc('get_home_data')
 
   if (error) return NextResponse.json({ error: true })
 
-  return NextResponse.json({ ...data, error: false })
+  console.log(data.products.filter((product: any) => product.id !== '8a163a56-2e8c-4eea-a569-ca376d35daa7'))
+
+  return NextResponse.json({
+    products: data.products.filter((product: any) => product.id !== '8a163a56-2e8c-4eea-a569-ca376d35daa7'),
+    influencers: data.influencers.filter((influencer: any) => influencer.full_name !== 'WestCOL'),
+    error: false
+  })
 }
